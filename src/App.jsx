baee5a1c5043113dc6d766e212c98d7cd28a4405
Jakob Kielland <jakob-ek@hotmail.com>
2022-05-08 20:42:29 +0200

    removed components from previous concept

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,9 @@ function App() {
   return (
     // Style this div to effectively change the body style
     <div className='h-screen overflow-auto'>
-      <div className='w-full h-full md:monitor-outer-bezel'>
-        <div className='w-full h-full md:monitor-inner-bezel'>
-          <div className='relative w-full h-full md:monitor-screen monitor-screen-mobile'>
-            <Routes>
-              <Route path='/' element={<HomeView />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route path='/' element={<HomeView />} />
+      </Routes>
     </div>
   );
 }
--- src/view/homeView.jsx
+++ src/view/homeView.jsx
@@ -1,15 +1,7 @@
-import LinePrint from '../components/text/linePrint';
-import WindowsHeaderBar from '../components/windowsHeaderBar';
-
 const HomeView = () => {
   return (
     <>
-      <div className='absolute flex flex-col w-5/6 gap-1 left-6 top-10 h-5/6 win98-window'>
-        <WindowsHeaderBar windowName='JakobKielland.exe' />
-        <div className='font-mono text-white bg-black win98-inner-border grow'>
-          <LinePrint content='Hello World!' />
-        </div>
-      </div>
+      <div className='absolute flex flex-col w-5/6 gap-1 left-6 top-10 h-5/6 win98-window'></div>
     </>
   );
 };
